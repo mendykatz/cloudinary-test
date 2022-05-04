bd2424b92ff3c2dd241b1c7495a2794e29b1b5fd
mendy <mendy.radix@gmail.com>
2022-05-04 13:06:10 +0300

    added photo-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PhotosListComponent } from './photos-list/photos-list.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbPopoverModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,16 @@ import { PhotosListComponent } from './photos-list/photos-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+    NbCardModule,
+    MatIconModule,
+    NbPopoverModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
